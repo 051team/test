16f1828b1785537a47c754e7a0e274934d32bab7
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-02 18:11:19 +0300

    casa de papel

---
+++ components/Homie.tsx
@@ -0,0 +1,13 @@
+import h from "../styles/Home.module.css";
+
+const Homie = () => {
+    return ( 
+        <div className={h.home}>
+            <div className={h.home_navbar}>
+
+            </div>
+        </div>
+     );
+}
+ 
+export default Homie;
