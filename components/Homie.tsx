1a0444275f8e667a385fc8cdf66de913371436bf
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-03 12:47:05 +0300

    Discord login works
    Navbar updated after successful login

--- components/Homie.tsx
+++ components/Homie.tsx
@@ -1,10 +1,24 @@
 import h from "../styles/Home.module.css";
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from "next/image";
 
 const Homie = () => {
+    const { data: session } = useSession();
+    if (session){
+        console.log(session);
+    }
     return ( 
         <div className={h.home}>
             <div className={h.home_navbar}>
-
+                    <button onClick={()=>signIn()}>
+                        <span>Sign In</span>
+                        {
+                            session && <Image src={session!.user!.image as string} alt={"discord profile image"} width={30} height={30} />
+                        }
+                        
+                        
+                    
+                    </button>
             </div>
         </div>
      );
