74e9ccce62c3cb903c5c8c00ee731ac8077919dc
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-03 17:45:29 +0300

    dropdown added
    animation added
    
    sign out to be activated

--- components/Homie.tsx
+++ components/Homie.tsx
@@ -2,11 +2,21 @@ import h from "../styles/Home.module.css";
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Image from "next/image";
 import _051 from "../public/051.jpg";
+import profile from "../public/profile.png";
+import logout from "../public/logout.png";
+import { useState } from "react";
 
 const Homie = () => {
     const { data: session } = useSession();
     if (session){
         console.log(session);
+    }
+    const handleLogInOut = () => {
+        if(!session){
+            signIn()
+        }else{
+            console.log("oturum açık")
+        }
     }
     return ( 
         <div className={h.home}>
@@ -18,7 +28,9 @@ const Homie = () => {
                     <Image src={_051} alt={"051 logo"} width={90} height={50} />
                     <input type="text" placeholder="Search for safe..." />
 
-                    <button onClick={()=>signIn()} id={h.profile}>
+                    <button onClick={handleLogInOut} id={h.profile}>
+                        <input type="checkbox" id="open"/>
+                        <label htmlFor="open"></label>
                         {
                             !session &&
                             <span>Sign In</span>
@@ -26,10 +38,22 @@ const Homie = () => {
                         {
                             session && 
                             <>
-                            <span>{session.user?.name}</span>
+                            <span> <strong>$ 00:00</strong> </span>
                             <Image src={session!.user!.image as string} alt={"discord profile image"} width={50} height={50} />
                             </>
                         }
+                        
+                        <div id={h.dropdown}>
+                            <button>
+                                <Image src={profile} alt={"profile icon"} width={25} height={25} />
+                                <span>Profile</span>
+                            </button>
+                            <button>
+                                <Image src={logout} alt={"logout icon"} width={25} height={25} />
+                                <span>Sign out</span>
+                            </button>
+                        </div>
+                        
                     </button>
                 </div>
             </div>
