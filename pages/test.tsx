375b634c6589490dd6f2d4b2002e2d80687e5ac0
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-25 22:13:10 +0300

    ...SSE fast version with res.end() only when req.on('close')

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,11 +2,9 @@ import { useState,useEffect } from "react";
 
 const Test = () => {
     const [gift,setGift] = useState<any>();
-    const [ES,setES] = useState<EventSource>();
 
     useEffect(() => {
         const eventSource = new EventSource('/api/sse');
-        setES(eventSource);
         eventSource.onmessage = (event) => {
             const eventData = JSON.parse(event.data);
             setGift(eventData);
@@ -22,7 +20,6 @@ const Test = () => {
     return ( 
         <div style={{width:"100vw", height:"100vh", display:"grid", justifyContent:"center",alignItems:"center", border:"3px solid red"}}>
             <h1>{gift && gift.giftName}</h1>
-            <button onClick={()=>ES?.close()}>Close</button>
         </div>
      );
 }
