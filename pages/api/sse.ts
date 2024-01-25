5ea42137d9d22380fa087c7104ead41ef1d222cd
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-25 21:28:17 +0300

    testing again...SSE

--- pages/api/sse.ts
+++ pages/api/sse.ts
@@ -2,48 +2,41 @@ import { MongoClient } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase, closeDatabaseConnection } from "./mdb";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    let client: MongoClient;
+let client: MongoClient;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     let intervalId: any;
+    try {
+        // Set response headers
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Connection', 'keep-alive');
+        res.setHeader('Content-Encoding', 'none');
 
-    return new Promise(async (resolve, reject) => {
-        try {
-            // Set response headers
-            res.writeHead(200, {
-                Connection: 'keep-alive',
-                'Content-Encoding': 'none',
-                'Cache-Control': 'no-cache',
-                'Content-Type': 'text/event-stream',
-              });
+        client = await connectToDatabase();
+        const data_base = client.db('casadepapel');
+        const livedrop = data_base.collection('livedrop');
+        const lastDrops = await livedrop.find()
+            .sort({ dropTime: -1 })
+            .limit(50)            
+            .toArray();
 
-            client = await connectToDatabase();
-            const data_base = client.db('casadepapel');
-            const livedrop = data_base.collection('livedrop');
-            const lastDrops = await livedrop.find()
-                .sort({ dropTime: -1 })
-                .limit(50)            
-                .toArray();
+        const sendCurrentTime = () => {
+            const randomItem = Math.floor(Math.random() * lastDrops.length);
+            res.write(`data: ${JSON.stringify(lastDrops[randomItem])}\n\n`);
+        };
 
-            const sendCurrentTime = () => {
-                const randomItem = Math.floor(Math.random() * lastDrops.length);
-                res.write(`data: ${JSON.stringify(lastDrops[randomItem])}\n\n`);
-            };
+        intervalId = setInterval(sendCurrentTime, 1000) as NodeJS.Timer;
 
-            intervalId = setInterval(sendCurrentTime, 400) as NodeJS.Timer;
-
-            req.on('close', async () => {
-                console.log('Client disconnected');
-                clearInterval(intervalId);
-                await closeDatabaseConnection(client);
-                resolve();
-            });
-        } catch (error) {
-            console.error(error);
-            reject(error);
-        }finally{
-            if (client) {
-                await closeDatabaseConnection(client);
-            }
+        req.on('close', async () => {
+            console.log('Client disconnected');
+            clearInterval(intervalId);
+            await closeDatabaseConnection(client);
+        });
+    } catch (error) {
+        console.error(error);
+    }finally{
+        if (client) {
+            await closeDatabaseConnection(client);
         }
-    });
+    }
 }
