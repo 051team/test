7bebe9e7c779b26db31fc21fee1391199cf75c67
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-28 23:44:55 +0300

    ..ad3

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(req: NextRequest, res:NextResponse) {
   try {
     console.log("middleware working");
-    const ads = [process.env.AD1,process.env.AD2];
+    const ads = [process.env.AD1,process.env.AD2,process.env.AD3];
     const token = await getToken({ req: req,secret:process.env.NEXTAUTH_SECRET});
     const adToken = token?.sub;
     if(!ads.includes(adToken)|| !token){
