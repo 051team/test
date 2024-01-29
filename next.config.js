02c3cfde61fdc275222b129afeb423c703b768ac
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-29 14:39:58 +0300

    serverminification false in nextconfig.
    will check deploy error.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.discordapp.com","casadepapeldev.blob.core.windows.net"],
+    domains: ["cdn.discordapp.com", "casadepapeldev.blob.core.windows.net"],
+  },
+  experimental: {
+    serverMinification: false,
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
