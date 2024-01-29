db5c0382fe76cc28cabf2257171e724725fc8ca7
codeyourwaytofreedom <can.kolej@gmail.com>
2024-01-29 14:44:47 +0300

    ...retest for defer

--- pages/defer/test.ts
+++ pages/defer/test.ts
@@ -1,32 +1,14 @@
 // the `defer()` helper will be used to define a background function
 import { defer } from "@defer/client";
-// If in Node.js, uncomment the following line:
-// import fetch from 'node-fetch';
 
+// a background function must be `async`
 async function triggerPump(name: string) {
-  const baseUrl = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:3000' 
-  : 'https://casadepapel.vercel.app';
-  setInterval(async () => {
-    try {
-      const response = await fetch(`${baseUrl}/api/pump`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error in fetch request:', error);
-    }
-  }, 50000);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Hello ${name}!`);
+      resolve("done");
+    }, 5000);
+  });
 }
 
 export default defer(triggerPump);
