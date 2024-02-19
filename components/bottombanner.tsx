693cd5f5d168a7c084d06fd8925f67d5a41091d7
051team <159935066+051team@users.noreply.github.com>
2024-02-20 00:35:50 +0300

    Update bottombanner.tsx

--- components/bottombanner.tsx
+++ components/bottombanner.tsx
@@ -6,18 +6,62 @@ import Link from "next/link";
 const BottomBanner = () => {
     return ( 
     <div id={bb.bottombanner}>
-        <Image src={_051} alt={"051 logo"} width={63} height={35} />
-        <div>
-            <Link href={"/"}>AFFILIATE PROGRAM </Link>
-            <Link href={"/"}>PROJECT PARTNERSHIP </Link>
-            <Link href={"/"}>CUSTOMER SUPPORT </Link>
-            <Link href={"/"}>PROVABLY FAIR </Link>
+        <div id={bb.shell}>
+            {/* <Image src={_051} alt={"051 logo"} width={63} height={35} /> */}
+            <div id={bb.leftcol}>
+                <Link href={"/"}>AFFILIATE PROGRAM </Link>
+                <Link href={"/"}>TERMS OF SERVICE </Link>
+                <Link href={"/"}>PROVABLY FAIR </Link>
+                <Link href={"/"}>SUPPORT</Link>
+                <Link href={"/"}>FAQ</Link>
+                <div id={bb.icons}>
+                    <Image id={bb.mad} src={"/discord.png"} alt={"discord"} width={20} height={20} />
+                    <Image id={bb.mad} src={"/x.png"} alt={"twitter"} width={20} height={20} />
+                    <Image id={bb.mad} src={"/discord.png"} alt={"discord"} width={20} height={20} />
+                    <Image id={bb.mad} src={"/x.png"} alt={"twitter"} width={20} height={20} />
+                </div>
+            </div>
+            <div id={bb.rightcol}>
+                <div id={bb.center}>
+                    <div id={bb.each}>
+                        <h2>12,435</h2>
+                        <h3 style={{color:"plum"}}>CASE OPENED</h3>
+                    </div>
+                    <div id={bb.each}>
+                        <h2>204,534</h2>
+                        <h3 style={{color:"salmon"}}>TOTAL VOLUME</h3>
+                    </div>
+                </div>
+                <div id={bb.ops}>
+                    <div id={bb.each}>
+                        <Image src={"/login/1_phantom.png"} alt={"2_solflare.png"} width={20} height={20} />
+                        <span>PHANTOM</span>
+                    </div>
+                    <div id={bb.each}>
+                        <Image src={"/login/2_solflare.png"} alt={"2_solflare.png"} width={20} height={20} />
+                        <span>SOLFLARE</span>
+                    </div>
+                    <div id={bb.each}>
+                        <Image src={"/login/4_metamask.png"} alt={"2_solflare.png"} width={20} height={20} />
+                        <span>META MASK</span>
+                    </div>
+                    <div id={bb.each}>
+                        <Image src={"/login/3_magiceden.png"} alt={"2_solflare.png"} width={20} height={20} />
+                        <span>MAGIC EDEN</span>
+                    </div>
+                    <div id={bb.each}>
+                        <Image src={"/login/backpack.png"} alt={"2_solflare.png"} width={20} height={20} />
+                        <span>BACKPACK</span>
+                    </div>
+                </div>
+                <Image id={bb.mad} src={"/bbanner/footer-nft.png"} alt={"mad"} width={270} height={300} />
+            </div>
         </div>
-        <div id={bb.right}>
-            <span>&#x2622;</span>
-            <span>&#x211A;</span>
-            <span>&#x213F;</span>
-            <span>&#x260E;</span>
+        <div id={bb.lowest}>
+            ALL RIGHTS RESERVED. POWERED BY 051.Io. COPYRIGHT © 2024
+            <span>
+                powered by &nbsp; <Image src={"/solana.png"} alt={"051 logo"} width={25} height={25} /> &nbsp; SOLANA
+            </span>
         </div>
     </div>
      );
