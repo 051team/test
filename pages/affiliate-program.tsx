0c6d9d271778bd35076e703f26c00f2243cd935c
051team <159935066+051team@users.noreply.github.com>
2024-02-27 01:54:09 +0300

    Update affiliate-program.tsx

--- pages/affiliate-program.tsx
+++ pages/affiliate-program.tsx
@@ -12,16 +12,16 @@ const _051 = () => {
                     </div>
                     <p>If you are a creator cooking something cool with NFTs on Solana.We want to cooperate with you!</p>
                     
-                    <strong>X Twitter</strong>
+                   <p><strong>X Twitter</strong></p>
                    <p>The minimum number of followers for a potential collaboration via a X profile should be a minimum of 3000 followers and 100 react under each tweet.</p>
                     
-                    <strong>Youtube - Twitch</strong>
+                   <p><strong>Youtube - Twitch</strong></p>
                    <p> In case of cooperation via Youtube, Twitch or any other platform, the channel must have a minimum of 500 subscriptions and 250 views under each video. </p>
                    <p> Every request for cooperation is being dealt with as an individual case or code. Collaboration also mainly depends on your activity on your channels and what content you share. Meeting all the conditions does not necessarily mean that we will cooperate with you. </p> 
                   
                  <p> If your channel or profile meets the above requirements or if you have additional questions, you can open a ticket in discord under the creator-request category.</p>
                     
-                    <strong>Benefits</strong>
+                    <p><strong>Benefits</strong></p>
                     <ul>
                         <li>Affiliate Revenue.</li>
                         <li>10% deposit bonus for Creator deposit code.</li>
