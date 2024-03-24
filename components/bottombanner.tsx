413d39dd7b4e9abe0c1be538a5566ce401fcb520
051team <159935066+051team@users.noreply.github.com>
2024-03-25 02:43:45 +0300

    Update bottombanner.tsx

--- components/bottombanner.tsx
+++ components/bottombanner.tsx
@@ -2,19 +2,22 @@ import bb from "./../styles/Wrapper1.module.css";
 import Image from "next/image";
 import _051 from "./../public/051.png";
 import Link from "next/link";
+import { formatter } from "../tools";
 
-const BottomBanner = () => {
+const BottomBanner = ({total}:any) => {
     return ( 
     <div id={bb.bottombanner}>
         <div id={bb.shell}>
             {/* <Image src={_051} alt={"051 logo"} width={63} height={35} /> */}
             <div id={bb.leftcol}>
-                <Link href={"/affiliate-program"}>AFFILIATE PROGRAM </Link>
-                <Link href={"/"}>TERMS OF SERVICE </Link>
-                <Link href={"/"}>PROVABLY FAIR </Link>
-                <Link href={"/"}>SUPPORT</Link>
-                <Link href={"https://whitepaper.051.io/getting-started/faq"}>FAQ</Link>
+                <Link href={"/051"}>AFFILIATE PROGRAM </Link>
+                <Link href={"/051"}>TERMS OF SERVICE </Link>
+                <Link href={"/051"}>PROVABLY FAIR </Link>
+                <Link href={"/051"}>SUPPORT</Link>
+                <Link href={"/051"}>FAQ</Link>
                 <div id={bb.icons}>
+                    <Image id={bb.mad} src={"/discord.png"} alt={"discord"} width={20} height={20} />
+                    <Image id={bb.mad} src={"/x.png"} alt={"twitter"} width={20} height={20} />
                     <Image id={bb.mad} src={"/discord.png"} alt={"discord"} width={20} height={20} />
                     <Image id={bb.mad} src={"/x.png"} alt={"twitter"} width={20} height={20} />
                 </div>
@@ -22,11 +25,11 @@ const BottomBanner = () => {
             <div id={bb.rightcol}>
                 <div id={bb.center}>
                     <div id={bb.each}>
-                        <h2>12,435</h2>
+                        <h2>{total && total.total}</h2>
                         <h3 style={{color:"plum"}}>CASE OPENED</h3>
                     </div>
                     <div id={bb.each}>
-                        <h2>204,534</h2>
+                    <h2>{total && formatter(total.turnover)}</h2>
                         <h3 style={{color:"salmon"}}>TOTAL VOLUME</h3>
                     </div>
                 </div>
@@ -56,9 +59,9 @@ const BottomBanner = () => {
             </div>
         </div>
         <div id={bb.lowest}>
-            ALL RIGHTS RESERVED. POWERED BY 051.io. COPYRIGHT © 2024
+            ALL RIGHTS RESERVED. POWERED BY 051.Io. COPYRIGHT © 2024
             <span>
-                powered by &nbsp; <Image src={"/solana.png"} alt={"051 logo"} width={25} height={25} />
+                powered by &nbsp; <Image src={"/solana.png"} alt={"051 logo"} width={25} height={25} /> &nbsp; SOLANA
             </span>
         </div>
     </div>
